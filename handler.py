6e85b350da22bf31781f81c416ad1e4aaaa9f3ac
Philipp Schmid <philipp@Philipps-MBP.fritz.box>
2019-07-16 21:53:31 +0200

    and again

--- handler.py
+++ handler.py
@@ -2,7 +2,6 @@ import subprocess
 import base64
 import json
 import datetime
-import os
 
 # os.system("cp ffmpeg /tmp") os.system("chmod 775 /tmp/ffmpeg")
 
@@ -17,12 +16,18 @@ def converter(event, context):
     # erstellt .m4a
     with open('/tmp/audio.m4a', 'wb') as file_:
         file_.write(decoded_audio)
+    file_.close()
+    # temp file
+    tmp_file = open('/tmp/fileout.flac', 'w+')
+    tmp_file.close()
+
     # converter es
     subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a", "-f",
                     "flac", "/tmp/fileout.flac", "-y"])
     # erstell flac
     with open("/tmp/fileout.flac", "rb") as audio_file:
         encoded_flac = base64.b64encode(audio_file.read())
+    audio_file.close()
     # removed file for new funktion call
     # erstellt response
     res = {
