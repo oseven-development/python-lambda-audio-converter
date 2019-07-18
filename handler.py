535b1739b839c8a8fa33fb62897ca7869f5df813
Max Hänsel <mh@maxhaensel.de>
2019-07-18 15:38:31 +0200

    File upload instead of base64

--- handler.py
+++ handler.py
@@ -6,7 +6,6 @@ import os
 import cgi
 from io import BytesIO
 
-
 # os.system("cp ffmpeg /tmp") os.system("chmod 775 /tmp/ffmpeg")
 
 FFMPEG_STATIC = "/var/task/ffmpeg"
@@ -18,69 +17,33 @@ def converter(event, context):
     body = event['body']
 
     ctype, pdict = cgi.parse_header(headers['Content-Type'])
-    # print(pdict)
-    pdict['CONTENT-LENGTH'] = int(headers['Content-Length'])
 
-    pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
+    if ctype == 'multipart/form-data':
+        pdict['CONTENT-LENGTH'] = int(headers['Content-Length'])
 
-    b = bytes(body, 'utf-8')
-    fields = cgi.parse_multipart(BytesIO(b), pdict)
-    # print(fields['data'][0])
-    out = open('audio.mp4', 'w+b')
-    out.write(fields['data'][0])
-    out.close()
-    print("-----------------------------")
-    # print(pdict)
+        pdict['boundary'] = bytes(pdict['boundary'], "utf-8")
 
-    # print(fields)
-    # if ctype == 'multipart/form-data':
-    #     fields = cgi.parse_multipart(body, pdict)
-    # messagecontent = fields.get('message')
+        b = bytes(body, 'iso-8859-1')
+        fields = cgi.parse_multipart(BytesIO(b), pdict)
 
+        out = open('audio.m4a', 'w+b')
+        out.write(fields['data'][0])
+        out.close()
 
-def converter2(event, context):
-    print(event)
-    # decode audio
-    # print(event)
-    req = json.loads(event)['body']['audio']
-    print(req)
-    # print(data)
-    # print(req)
-    # print(type(req))
-    # print(req)
-    # print(event['body']['body'])
-    # print(event['body'])
-    decoded_audio = base64.b64decode(req)
-    # erstellt .m4a
-    with open('/tmp/audio.m4a', 'wb') as file_:
-        file_.write(decoded_audio)
-    file_.close()
-    # temp file
-    tmp_file = open('/tmp/fileout.flac', 'w+')
-    tmp_file.close()
+        tmp_file = open('fileout.flac', 'w+')
+        tmp_file.close()
 
-    # converter es
-    subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a", "-f",
-                    "flac", "/tmp/fileout.flac", "-y"])
-    # erstell flac
-    with open("/tmp/fileout.flac", "rb") as audio_file:
-        encoded_flac = base64.b64encode(audio_file.read())
-    audio_file.close()
-    # removed file for new funktion call
-    # erstellt response
-    res = {
+        subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a",
+                        "-f", "flac", "/tmp/fileout.flac", "-y"])
+
+        # os.remove("/tmp/fileout.flac")
+        # os.remove("/tmp/audio.m4a")
+
+    return {
         "statusCode": 200,
         "headers": {
             "Content-Type": "application/json",
             "Access-Control-Allow-Origin": "*"
         },
-        "body": json.dumps(encoded_flac.decode("utf-8"))
+        "body": "file"
     }
-    os.remove("/tmp/fileout.flac")
-    os.remove("/tmp/audio.m4a")
-
-    # print(res)
-    return res
-
-
-#
