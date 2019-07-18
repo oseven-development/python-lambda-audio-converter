7ae958e9c25a07f295a2e239ce08d7ebe75d229f
Philipp Schmid <philipp@Philipps-MBP.fritz.box>
2019-07-18 18:29:55 +0200

    added ffmpeg

--- handler.py
+++ handler.py
@@ -26,24 +26,80 @@ def converter(event, context):
         b = bytes(body, 'iso-8859-1')
         fields = cgi.parse_multipart(BytesIO(b), pdict)
 
-        out = open('audio.m4a', 'w+b')
+        out = open('/tmp/audio.m4a', 'w+b')
         out.write(fields['data'][0])
         out.close()
 
-        tmp_file = open('fileout.flac', 'w+')
+        tmp_file = open('/tmp/fileout.flac', 'w+')
         tmp_file.close()
 
-        subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a",
-                        "-f", "flac", "/tmp/fileout.flac", "-y"])
+    subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a",
+                "-f", "flac", "/tmp/fileout.flac", "-y"])
 
-        # os.remove("/tmp/fileout.flac")
-        # os.remove("/tmp/audio.m4a")
+    res = {
+          "statusCode": 200,
+          "headers": {
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*"
+          },
+          "body": json.dumps(encoded_flac.decode("utf-8"))
+      }
+    os.remove("/tmp/fileout.flac")
+    os.remove("/tmp/audio.m4a")
 
-    return {
-        "statusCode": 200,
-        "headers": {
-            "Content-Type": "application/json",
-            "Access-Control-Allow-Origin": "*"
-        },
-        "body": "file"
-    }
+    return res
+
+# os.system("cp ffmpeg /tmp") os.system("chmod 775 /tmp/ffmpeg")
+
+
+
+    # print(form_data)
+
+    # print(file)
+    # with open("request.txt", "w+") as audio_file:
+    #     audio_file.write(json.dumps(event))
+    
+    # with open("request.m4a", "wb") as audio_file:
+    #     audio_file.write(event['body'])
+    # data = json.loads(event['body'])
+    # Let's say we user a regular <input type='file' name='uploaded_file'/>
+    # encoded_file = data['uploaded_file']
+    # decoded_file = base64.decodestring(encoded_file)
+
+    # req = json.loads(event)['body']['audio']
+    # print(req)
+    # print(data)
+    # print(req)
+    # print(type(req))
+    # print(req)
+    # print(event['body']['body'])
+    # print(event['body'])
+    # decoded_audio = base64.b64decode(req)
+    # erstellt .m4a
+    # with open('/tmp/audio.m4a', 'wb') as file_:
+    #     file_.write(decoded_audio)
+    # file_.close()
+    # temp file
+    # tmp_file = open('/tmp/fileout.flac', 'w+')
+    # tmp_file.close()
+
+    # # converter es
+    # subprocess.run([FFMPEG_STATIC, "-i", "/tmp/audio.m4a", "-f",
+    #                 "flac", "/tmp/fileout.flac", "-y"])
+    # # erstell flac
+    # with open("/tmp/fileout.flac", "rb") as audio_file:
+    #     encoded_flac = base64.b64encode(audio_file.read())
+    # audio_file.close()
+    # # removed file for new funktion call
+    # # erstellt response
+    # print(encoded_flac)
+    # res = {
+    #     "statusCode": 200,
+    #     "headers": {
+    #         "Content-Type": "application/json",
+    #         "Access-Control-Allow-Origin": "*"
+    #     },
+    #     "body": json.dumps(encoded_flac.decode("utf-8"))
+    # }
+    # os.remove("/tmp/fileout.flac")
+    # os.remove("/tmp/audio.m4a")
